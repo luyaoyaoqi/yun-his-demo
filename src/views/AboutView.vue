e9759bd8da7725f4a43f5c46c676c62f9deb42a7
luyao <9291499+luyaoyaoqi@user.noreply.gitee.com>
2025-06-18 11:31:51 +0800

    加入暗黑模式控制

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,25 +1,27 @@
 <template>
-<div class="bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
-  <div>
-    <span class="inline-flex items-center justify-center rounded-md bg-indigo-500 p-2 shadow-lg">
-      <svg
-        class="h-6 w-6 text-white"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        aria-hidden="true"
-      >
-        <!-- ... -->
-      </svg>
-    </span>
+  <div class="bg-white dark:bg-gray-800 rounded-lg px-6 py-8 ring shadow-xl ring-gray-900/5">
+    <div>
+      <span class="inline-flex items-center justify-center rounded-md bg-indigo-500 dark:bg-indigo-400 p-2 shadow-lg">
+        <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+         
+        </svg>
+      </span>
+    </div>
+    <h3 class="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight ">Writes upside-down</h3>
+    <p class="text-gray-500 dark:text-gray-400 mt-2 text-sm ">
+      The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
+    </p>
+    <el-button @click="toggleDark()" class="!my-[16px]">{{ isDark ? 'Dark' : 'Light' }}</el-button>
   </div>
-  <h3 class="text-gray-900 dark:text-white mt-5 text-base font-medium tracking-tight ">Writes upside-down</h3>
-  <p class="text-gray-500 dark:text-gray-400 mt-2 text-sm ">
-    The Zero Gravity Pen can be used to write in any orientation, including upside-down. It even works in outer space.
-  </p>
-</div>
 
 </template>
+
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+
+const isDark = useDark({})
+const toggleDark = useToggle(isDark)
+</script>
 
 <style>
 @media (min-width: 1024px) {
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -13,6 +17,12 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
     tailwindcss(),
+     AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {

5034a73bab34b7631fe6745653dfd74aec21d56d
luyao <9291499+luyaoyaoqi@user.noreply.gitee.com>
2025-09-15 10:09:28 +0800

    feat(DatePicker): 添加双日期选择功能
    - 在 DatePicker 组件中添加了第二个日期选择器
    - 实现了日期范围选择功能，包括"今天"、"今年"

--- components.d.ts
+++ components.d.ts
@@ -15,6 +15,7 @@ declare module 'vue' {
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElDialog: typeof import('element-plus/es')['ElDialog']
+    ElDivider: typeof import('element-plus/es')['ElDivider']
     ElForm: typeof import('element-plus/es')['ElForm']
     ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElInput: typeof import('element-plus/es')['ElInput']
--- src/views/DatePicker.vue
+++ src/views/DatePicker.vue
@@ -1,5 +1,8 @@
 <template>
     <DatePickerAndSelect v-model:date="date" v-model:year="year" v-model:showElDatePicker="showElDatePicker" />
+    <el-divider />
+    <el-date-picker popper-class="mdd-el-date-picker-popper" v-model="value2" type="daterange" unlink-panels
+        range-separator="To" start-placeholder="Start month" end-placeholder="End month" :shortcuts="shortcuts" />
 </template>
 
 <script setup>
@@ -21,4 +24,51 @@ watch([date, year, showElDatePicker], ([newDate, newYear, newShowElDatePicker], 
         console.log('showElDatePicker changed', newShowElDatePicker)
     }
 })
+
+const value2 = ref('')
+const shortcuts = [
+    {
+        text: '今天',
+        value: [new Date(), new Date()],
+    },
+    {
+        text: '今年',
+        value: () => {
+            const end = new Date()
+            const start = new Date(new Date().getFullYear(), 0)
+            return [start, end]
+        },
+    },
+    {
+        text: '最近6个月',
+        value: () => {
+            const end = new Date()
+            const start = new Date()
+            start.setMonth(start.getMonth() - 6)
+            return [start, end]
+        },
+    },
+]
 </script>
+<style lang="less">
+.mdd-el-date-picker-popper {
+    .el-picker-panel__body-wrapper {
+        .el-picker-panel__sidebar {
+            width: 100%;
+            position: relative;
+            order: 2;
+            display: flex;
+            padding: 0 20px;
+            .el-picker-panel__shortcut{
+                width: fit-content;
+                padding: 0 0 8px;
+                margin-right: 24px;
+            }
+        }
+
+        .el-picker-panel__body {
+            margin-left: 0;
+        }
+    }
+}
+</style>

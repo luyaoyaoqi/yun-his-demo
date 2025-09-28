36ba739674c9eec7c334b270db28fcf6f935621b
luyao <9291499+luyaoyaoqi@user.noreply.gitee.com>
2025-09-28 17:23:00 +0800

    feat(components): 添加 ElButtonGroup 和 ElCascader 组件类型声明
    新增了 Element Plus 的 ElButtonGroup 和 ElCascader 组件的类型声明，
    以便在项目中正确使用这些组件并获得完整的类型支持。
    
    ---
    
    feat(router): 添加 /mddCascader 路由配置
    
    为 MddCascader 页面组件添加新的路由配置，支持代码分割和懒加载。
    
    ---
    
    feat(menu): 在导航菜单中增加 MddCascader 入口
    
    在顶部导航菜单中新增指向 /mddCascader 路径的菜单项，方便访问级联选择器示例页面。
    
    ---
    
    style(css): 增加隐藏下拉箭头样式及打印相关样式调整
    
    添加 CSS 规则以隐藏弹出框的箭头，并对打印编辑区域样式做了相应调整。

--- src/router/index.ts
+++ src/router/index.ts
@@ -121,6 +121,14 @@ const router = createRouter({
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/DatePicker.vue'),
     },
+    {
+      path: '/mddCascader',
+      name: 'mddCascader',
+      // route level code-splitting
+      // this generates a separate chunk (Input.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/MddCascader.vue'),
+    },
   ],
 })
 
---
+++ src/views/MddCascader.vue
@@ -0,0 +1,339 @@
+<template>
+    <div class="w-[600px]">
+        <el-cascader ref="cascader" class="w-full" v-model="value" :options="options" :props="props"
+            :collapse-tags="true" :max-collapse-tags="1" :filterable="true">
+            <template #header>
+                <el-input v-model="searchValue" placeholder="Please select" @input="handleSearchChange" />
+            </template>
+            <template #prefix>
+                <span class="text-[var(--el-text-color-regular)]">{{ displayText }}</span>
+            </template>
+            <template #footer>
+                <div class="flex justify-end">
+                    <el-button plain @click="handleClear">清空</el-button>
+                    <el-button type="primary" @click="handleConfirm">确定</el-button>
+                </div>
+            </template>
+        </el-cascader>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, computed } from 'vue'
+
+const value = ref([])
+const cascader = ref()
+
+const props = {
+    // expandTrigger: 'hover' as const,
+    multiple: true,
+
+}
+
+const handleSearchChange = (value: string) => {
+    console.log(value)
+
+    // 通过 DOM 操作
+    const searchInput = cascader.value?.$el.nextElementSibling.querySelector('.el-cascader__search-input') as HTMLInputElement
+    console.log(searchInput)
+    if (searchInput) {
+        searchInput.value = value
+        //触发 input 事件
+        searchInput.dispatchEvent(new Event('input'))
+    }
+
+}
+
+const searchValue = ref('')
+
+const options = [
+    {
+        value: 'guide',
+        label: 'Guide',
+        children: [
+            {
+                value: 'disciplines',
+                label: 'Disciplines',
+                children: [
+                    {
+                        value: 'consistency',
+                        label: 'Consistency',
+                    },
+                    {
+                        value: 'feedback',
+                        label: 'Feedback',
+                    },
+                    {
+                        value: 'efficiency',
+                        label: 'Efficiency',
+                    },
+                    {
+                        value: 'controllability',
+                        label: 'Controllability',
+                    },
+                ],
+            },
+            {
+                value: 'navigation',
+                label: 'Navigation',
+                children: [
+                    {
+                        value: 'side nav',
+                        label: 'Side Navigation',
+                    },
+                    {
+                        value: 'top nav',
+                        label: 'Top Navigation',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        value: 'component',
+        label: 'Component',
+        children: [
+            {
+                value: 'basic',
+                label: 'Basic',
+                children: [
+                    {
+                        value: 'layout',
+                        label: 'Layout',
+                    },
+                    {
+                        value: 'color',
+                        label: 'Color',
+                    },
+                    {
+                        value: 'typography',
+                        label: 'Typography',
+                    },
+                    {
+                        value: 'icon',
+                        label: 'Icon',
+                    },
+                    {
+                        value: 'button',
+                        label: 'Button',
+                    },
+                ],
+            },
+            {
+                value: 'form',
+                label: 'Form',
+                children: [
+                    {
+                        value: 'radio',
+                        label: 'Radio',
+                    },
+                    {
+                        value: 'checkbox',
+                        label: 'Checkbox',
+                    },
+                    {
+                        value: 'input',
+                        label: 'Input',
+                    },
+                    {
+                        value: 'input-number',
+                        label: 'InputNumber',
+                    },
+                    {
+                        value: 'select',
+                        label: 'Select',
+                    },
+                    {
+                        value: 'cascader',
+                        label: 'Cascader',
+                    },
+                    {
+                        value: 'switch',
+                        label: 'Switch',
+                    },
+                    {
+                        value: 'slider',
+                        label: 'Slider',
+                    },
+                    {
+                        value: 'time-picker',
+                        label: 'TimePicker',
+                    },
+                    {
+                        value: 'date-picker',
+                        label: 'DatePicker',
+                    },
+                    {
+                        value: 'datetime-picker',
+                        label: 'DateTimePicker',
+                    },
+                    {
+                        value: 'upload',
+                        label: 'Upload',
+                    },
+                    {
+                        value: 'rate',
+                        label: 'Rate',
+                    },
+                    {
+                        value: 'form',
+                        label: 'Form',
+                    },
+                ],
+            },
+            {
+                value: 'data',
+                label: 'Data',
+                children: [
+                    {
+                        value: 'table',
+                        label: 'Table',
+                    },
+                    {
+                        value: 'tag',
+                        label: 'Tag',
+                    },
+                    {
+                        value: 'progress',
+                        label: 'Progress',
+                    },
+                    {
+                        value: 'tree',
+                        label: 'Tree',
+                    },
+                    {
+                        value: 'pagination',
+                        label: 'Pagination',
+                    },
+                    {
+                        value: 'badge',
+                        label: 'Badge',
+                    },
+                ],
+            },
+            {
+                value: 'notice',
+                label: 'Notice',
+                children: [
+                    {
+                        value: 'alert',
+                        label: 'Alert',
+                    },
+                    {
+                        value: 'loading',
+                        label: 'Loading',
+                    },
+                    {
+                        value: 'message',
+                        label: 'Message',
+                    },
+                    {
+                        value: 'message-box',
+                        label: 'MessageBox',
+                    },
+                    {
+                        value: 'notification',
+                        label: 'Notification',
+                    },
+                ],
+            },
+            {
+                value: 'navigation',
+                label: 'Navigation',
+                children: [
+                    {
+                        value: 'menu',
+                        label: 'Menu',
+                    },
+                    {
+                        value: 'tabs',
+                        label: 'Tabs',
+                    },
+                    {
+                        value: 'breadcrumb',
+                        label: 'Breadcrumb',
+                    },
+                    {
+                        value: 'dropdown',
+                        label: 'Dropdown',
+                    },
+                    {
+                        value: 'steps',
+                        label: 'Steps',
+                    },
+                ],
+            },
+            {
+                value: 'others',
+                label: 'Others',
+                children: [
+                    {
+                        value: 'dialog',
+                        label: 'Dialog',
+                    },
+                    {
+                        value: 'tooltip',
+                        label: 'Tooltip',
+                    },
+                    {
+                        value: 'popover',
+                        label: 'Popover',
+                    },
+                    {
+                        value: 'card',
+                        label: 'Card',
+                    },
+                    {
+                        value: 'carousel',
+                        label: 'Carousel',
+                    },
+                    {
+                        value: 'collapse',
+                        label: 'Collapse',
+                    },
+                ],
+            },
+        ],
+    },
+    {
+        value: 'resource',
+        label: 'Resource',
+        children: [
+            {
+                value: 'axure',
+                label: 'Axure Components',
+            },
+            {
+                value: 'sketch',
+                label: 'Sketch Templates',
+            },
+            {
+                value: 'docs',
+                label: 'Design Documentation',
+            },
+        ],
+    },
+]
+
+const handleConfirm = () => {
+    cascader.value?.togglePopperVisible()
+}
+const handleClear = () => {
+    value.value = []
+}
+
+const displayText = computed(() => {
+    if (!value.value || value.value.length === 0) {
+        return ''
+    }
+    const firstValue = value.value[0]
+    const firstPart = Array.isArray(firstValue) ? firstValue[0] : firstValue.split(',')[0]
+    return `${firstPart}等${value.value.length}种`
+})
+</script>
+
+<style lang="less" scoped>
+:deep(.el-cascader__tags) {
+    display: none;
+}
+</style>

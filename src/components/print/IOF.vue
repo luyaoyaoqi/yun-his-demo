c845fd6e8a64ad80589fe72502002b9c0709df53
luyaoyaoqi <962333639@qq.com>
2026-02-05 18:09:12 +0800

    基础完成

--- src/components/print/IOF.vue
+++ src/components/print/IOF.vue
@@ -155,12 +155,12 @@
                                                 <div class="medicine-name">
                                                     {{ child.medicine.info.name }}
                                                     <span class="product-name">{{ child.medicine.info.productName
-                                                    }}</span>
+                                                        }}</span>
                                                     (
                                                     <span class="medicine-spec">{{ child.medicine.info.spec }}</span>
                                                     ×
                                                     <span class="medicine-quantity">{{ child.medicine.info.quantity
-                                                    }}</span>
+                                                        }}</span>
                                                     )
                                                 </div>
                                                 <!-- 皮试没有不显示 -->
@@ -310,13 +310,14 @@ const props = defineProps({
 });
 let data: any = {};
 
-// 初始测试
+
+// 输液注射单：成药处方
 // data = {
-//     title: '输液注射单',
-//     signatureRight: false,
+//     title: '输液单',
+//     signatureRight: true,
 //     signatureShow: true,
-//     treatmentDataShow: true,
-//     infusionDataShowOther: true,
+//     treatmentDataShow: false,
+//     infusionDataShowOther: false,
 //     treatmentData: [{
 //         medicineName: '针灸',
 //         medicineQuantity: '0/2根'
@@ -326,7 +327,7 @@ let data: any = {};
 //     }],
 //     infusionData: [{
 //         isOther: false,
-//         groupIndex: '①',
+//         groupIndex: '',
 //         groupChildren: [{
 //             medicine: {
 //                 info: {
@@ -339,18 +340,6 @@ let data: any = {};
 //                 dosage: '250ml',
 //                 manufacturer: '北京韩美'
 //             },
-//         }, {
-//             medicine: {
-//                 info: {
-//                     name: '克林霉素磷酸酯注射液',
-//                     productName: '',
-//                     spec: '0.3g*2ml/支',
-//                     quantity: '4支',
-//                 },
-//                 test: '',
-//                 dosage: '1.2g',
-//                 manufacturer: '北京韩美'
-//             },
 //         }],
 //         instruction: {
 //             dose: '',
@@ -360,8 +349,31 @@ let data: any = {};
 //             speed: '60滴/分钟'
 //         }
 //     }, {
+//         isOther: true,
+//         groupIndex: '',
+//         groupChildren: [{
+//             medicine: {
+//                 info: {
+//                     name: '鼻炎喷雾',
+//                     productName: '',
+//                     spec: '1瓶/瓶',
+//                     quantity: '1瓶',
+//                 },
+//                 test: '',
+//                 dosage: '',
+//                 manufacturer: ''
+//             },
+//         }],
+//         instruction: {
+//             dose: '每次1瓶',
+//             frequency: '每天1次',
+//             days: '7天',
+//             method: '滴鼻',
+//             speed: ''
+//         }
+//     }, {
 //         isOther: false,
-//         groupIndex: '②',
+//         groupIndex: '',
 //         groupChildren: [{
 //             medicine: {
 //                 info: {
@@ -372,18 +384,6 @@ let data: any = {};
 //                 },
 //                 test: '',
 //                 dosage: '500ml',
-//                 manufacturer: '北京韩美'
-//             },
-//         }, {
-//             medicine: {
-//                 info: {
-//                     name: '维生素C注射液',
-//                     productName: '',
-//                     spec: '0.5g*2ml/支',
-//                     quantity: '3支',
-//                 },
-//                 test: '',
-//                 dosage: '1.2g',
 //                 manufacturer: '北京韩美'
 //             },
 //         }],
@@ -397,10 +397,10 @@ let data: any = {};
 //     }],
 // }
 
-// 输液注射单：成药处方
+// 输液注射单：输注处方
 data = {
     title: '输液注射单',
-    signatureRight: false,
+    signatureRight: true,
     signatureShow: true,
     treatmentDataShow: true,
     infusionDataShowOther: true,
@@ -413,7 +413,7 @@ data = {
     }],
     infusionData: [{
         isOther: false,
-        groupIndex: '',
+        groupIndex: '①',
         groupChildren: [{
             medicine: {
                 info: {
@@ -426,6 +426,18 @@ data = {
                 dosage: '250ml',
                 manufacturer: '北京韩美'
             },
+        }, {
+            medicine: {
+                info: {
+                    name: '克林霉素磷酸酯注射液',
+                    productName: '',
+                    spec: '0.3g*2ml/支',
+                    quantity: '4支',
+                },
+                test: '',
+                dosage: '1.2g',
+                manufacturer: '北京韩美'
+            },
         }],
         instruction: {
             dose: '',
@@ -435,31 +447,8 @@ data = {
             speed: '60滴/分钟'
         }
     }, {
-        isOther: true,
-        groupIndex: '',
-        groupChildren: [{
-            medicine: {
-                info: {
-                    name: '鼻炎喷雾',
-                    productName: '',
-                    spec: '1瓶/瓶',
-                    quantity: '1瓶',
-                },
-                test: '',
-                dosage: '',
-                manufacturer: ''
-            },
-        }],
-        instruction: {
-            dose: '每次1瓶',
-            frequency: '每天1次',
-            days: '7天',
-            method: '滴鼻',
-            speed: ''
-        }
-    }, {
         isOther: false,
-        groupIndex: '',
+        groupIndex: '②',
         groupChildren: [{
             medicine: {
                 info: {
@@ -472,6 +461,18 @@ data = {
                 dosage: '500ml',
                 manufacturer: '北京韩美'
             },
+        }, {
+            medicine: {
+                info: {
+                    name: '维生素C注射液',
+                    productName: '',
+                    spec: '0.5g*2ml/支',
+                    quantity: '3支',
+                },
+                test: '',
+                dosage: '1.2g',
+                manufacturer: '北京韩美'
+            },
         }],
         instruction: {
             dose: '',
@@ -482,6 +483,124 @@ data = {
         }
     }],
 }
+
+//注射单
+// data = {
+//     title: '注射单',
+//     signatureRight: false,
+//     signatureShow: true,
+//     treatmentDataShow: false,
+//     infusionDataShowOther: false,
+//     treatmentData: [],
+//     infusionData: [{
+//         isOther: false,
+//         groupIndex: '③',
+//         groupChildren: [{
+//             medicine: {
+//                 info: {
+//                     name: '注射用奥美拉唑钠',
+//                     productName: '',
+//                     spec: '40mg',
+//                     quantity: '3支',
+//                 },
+//                 test: '',
+//                 dosage: '1支',
+//                 manufacturer: '湖南五洲通药业有限责任公司'
+//             },
+//         }, {
+//             medicine: {
+//                 info: {
+//                     name: '注射用头孢曲松钠',
+//                     productName: '',
+//                     spec: '1g',
+//                     quantity: '3瓶',
+//                 },
+//                 test: '',
+//                 dosage: '1瓶',
+//                 manufacturer: '广州白云山天心制药股份有限公司'
+//             },
+//         }],
+//         instruction: {
+//             dose: '',
+//             frequency: '每天1次',
+//             days: '3天',
+//             method: '肌肉注射'
+//         }
+//     }],
+// }
+
+//雾化单
+// data = {
+//     title: '雾化单',
+//     signatureRight: true,
+//     signatureShow: true,
+//     treatmentDataShow: false,
+//     infusionDataShowOther: false,
+//     treatmentData: [],
+//     infusionData: [{
+//         isOther: false,
+//         groupIndex: '④',
+//         groupChildren: [{
+//             medicine: {
+//                 info: {
+//                     name: '鼻朗生理性海水鼻腔喷',
+//                     productName: '',
+//                     spec: '',
+//                     quantity: '3盒',
+//                 },
+//                 test: '',
+//                 dosage: '1瓶',
+//                 manufacturer: '浙江朗柯生物工程有限公司'
+//             },
+//         }],
+//         instruction: {
+//             dose: '',
+//             frequency: '每天1次',
+//             days: '3天',
+//             method: '雾化吸入'
+//         }
+//     }],
+// }
+
+//外治单
+// data = {
+//     title: '外治单',
+//     signatureRight: true,
+//     signatureShow: true,
+//     treatmentDataShow: true,
+//     infusionDataShowOther: true,
+//     treatmentData: [{
+//         medicineName: '针灸',
+//         medicineQuantity: '0/2根'
+//     }, {
+//         medicineName: '补牙',
+//         medicineQuantity: '0/2次'
+//     }],
+//     infusionData: [{
+//         isOther: true,
+//         groupIndex: '',
+//         groupChildren: [{
+//             medicine: {
+//                 info: {
+//                     name: '鼻炎喷雾',
+//                     productName: '',
+//                     spec: '1瓶/瓶',
+//                     quantity: '1瓶',
+//                 },
+//                 test: '',
+//                 dosage: '',
+//                 manufacturer: ''
+//             },
+//         }],
+//         instruction: {
+//             dose: '每次1瓶',
+//             frequency: '每天1次',
+//             days: '7天',
+//             method: '滴鼻',
+//             speed: ''
+//         }
+//     }],
+// }
 
 </script>
 
@@ -741,12 +860,36 @@ data = {
                     align-items: center;
 
                     .infusion-content {
+                        display: flex;
+                        flex-wrap: wrap;
+
                         .dose {
                             display: none;
+                            order: 1;
                         }
 
-                        >span:not(:last-child) {
-                            margin-right: 4pt;
+                        .frequency {
+                            order: 2;
+                        }
+
+                        .days {
+                            order: 3;
+                        }
+
+                        .method {
+                            order: 4;
+                        }
+
+                        .speed {
+                            order: 5;
+                        }
+
+                        >span {
+                            white-space: nowrap;
+
+                            &:not(:last-child) {
+                                margin-right: 4pt;
+                            }
                         }
                     }
                 }
@@ -783,7 +926,7 @@ data = {
                         align-items: flex-start;
 
                         .infusion-content {
-                            white-space: nowrap;
+                            flex-wrap: nowrap;
 
                             .dose {
                                 display: inline;
@@ -793,8 +936,12 @@ data = {
                                 display: none;
                             }
 
+                            .days {
+                                margin-right: 0;
+                            }
+
                             .method {
-                                order: -1;
+                                order: 2;
                             }
                         }
                     }
@@ -853,7 +1000,9 @@ data = {
                         margin-left: 16pt;
 
                         .infusion-content {
-                            display: flex;
+                            .method {
+                                order: 0;
+                            }
 
                             br {
                                 display: none;
